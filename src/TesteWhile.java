e8e9b66e35ee71ab12e579cfdbd033487b32d6a7
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-02-20 18:54:42 -0300

    TestarWhile

--- src/TesteWhile.java
+++ src/TesteWhile.java
@@ -1,5 +1,11 @@
 public class TesteWhile {
     public static void main(String[] args) {
+        int contador = 0;
+
+        while (contador <= 10) {
+            System.out.println(contador);
+            contador+=1;
+        }
 
     }
 }

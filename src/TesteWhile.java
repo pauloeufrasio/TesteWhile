1f274695062e32e8f75aaf299794c1f6daf0e252
paulo <henriquepaulo0606@gmail.com>
2023-03-25 10:28:08 -0300

    Testando while contando ate 10

--- src/TesteWhile.java
+++ src/TesteWhile.java
@@ -1,11 +1,11 @@
 public class TesteWhile {
     public static void main(String[] args) {
+        // Inicializando a variavel contador
         int contador = 0;
-
+        // enquanto contador for menor igual a 10 ele irá contar a ate 10
         while (contador <= 10) {
             System.out.println(contador);
-            //Contador = contador + 1;
-            // contador+=1;
+        // Incrementando o que está dentro dele mais um
             contador++;
         }
 
